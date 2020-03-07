66eafadb3625e00a18443ba6d08934f940922a79
ThatJohn <fajardocj@gmail.com>
2020-03-07 14:44:30 -0500

    Incremental save

--- backend/routes/forms/index.js
+++ backend/routes/forms/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { Forms } = require('../../models');
 
-router.get('/', async (req, res) => {
+router.get('/forms', async (req, res) => {
   res.json(await Form.all());
 });
 
-router.post('/', cors(), async (req, res) => {
+router.post('/new', cors(), async (req, res) => {
   res.json(await Form.new(req));
 });
 
