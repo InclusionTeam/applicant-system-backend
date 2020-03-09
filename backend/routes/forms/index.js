9a38343f57a137af0ad9751008613e14c907c05f
John Fajardo <fajardoc.j@gmail.com>
2020-03-09 19:23:53 -0400

    Incremental save

--- backend/routes/forms/index.js
+++ backend/routes/forms/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Reservation } = require('../../models');
 
 router.get('/', async (req,res) => {
-  res.json(await Reservation.all());
+  res.json(await Form.all());
 });
 
 module.exports = router;
